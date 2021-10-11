7384bd75848e990c1d3095e4773fbb21780e10bc
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-10-11 12:09:24 -0300

    ✨ (strapi): add strapi with postgress configuration

---
+++ strapi/reactavancado/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c732e18835589ce66b46310890f8d3dc'),
+    },
+  },
+});
---
+++ strapi/reactavancado/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c96e5439-a6a3-4538-8feb-a4f4c6ecd250'
+};
